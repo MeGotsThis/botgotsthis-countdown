31602d57c963d8c5e4e84905b7e448c30c0faba0
MeGotsThis <megotsthis@gmail.com>
2017-09-28 12:21:28 -0700

    Adding files

---
+++ channel.py
@@ -0,0 +1,67 @@
+﻿import asyncio
+from datetime import timedelta
+
+from bot import data  # noqa: F401
+from lib.data import ChatCommandArgs
+from lib.helper.chat import feature, permission
+
+
+@feature('countdown')
+@permission('moderator')
+async def commandCountdown(args: ChatCommandArgs) -> bool:
+    """
+    !countdown 1
+    """
+
+    if 'countdownTask' in args.chat.sessionData:
+        return True
+
+    delay: timedelta
+    if 'countdown-' in args.message.command:
+        try:
+            delay = timedelta(
+                seconds=float(args.message.command.split('countdown-')[1]))
+        except (ValueError, IndexError):
+            delay = timedelta()
+    else:
+        delay = timedelta()
+
+    duration: timedelta
+    try:
+        duration = timedelta(seconds=float(args.message[1]))
+    except (ValueError, IndexError):
+        duration = timedelta(seconds=10)
+
+    if not args.permissions.twitchAdmin:
+        delay = min(delay, timedelta(seconds=60))
+        duration = min(duration, timedelta(seconds=300))
+
+    if not args.permissions.broadcaster:
+        delay = min(delay, timedelta(seconds=30))
+        duration = min(duration, timedelta(seconds=30))
+
+    if not duration:
+        return True
+
+    coro = countdown_task(chat=args.chat, delay=delay, duration=duration)
+    args.chat.sessionData['countdownTask'] = coro
+    asyncio.ensure_future(coro)
+
+    return True
+
+
+async def countdown_task(
+        chat: 'data.Channel',
+        delay: timedelta,
+        duration: timedelta) -> None:
+    loop: asyncio.AbstractEventLoop = asyncio.get_event_loop()
+    asyncio.sleep(delay.total_seconds())
+    lapse: int = int(round(duration.total_seconds()))
+    start: float = loop.time()
+    d: int
+    for d in range(lapse, 0, -1):
+        chat.send(str(d))
+        await asyncio.sleep(start + 1 - loop.time())
+        start += 1
+    chat.send('Go!')
+    del chat.sessionData['countdownTask']
---
+++ items/channel.py
@@ -0,0 +1,31 @@
+﻿from typing import Iterable, Mapping, Optional
+
+from lib.data import ChatCommand
+
+from .. import channel
+
+
+def filterMessage() -> Iterable[ChatCommand]:
+    return []
+
+
+def commands() -> Mapping[str, Optional[ChatCommand]]:
+    if not hasattr(commands, 'commands'):
+        setattr(commands, 'commands', {
+            '!countdown': channel.commandCountdown,
+            }
+        )
+    return getattr(commands, 'commands')
+
+
+def commandsStartWith() -> Mapping[str, Optional[ChatCommand]]:
+    if not hasattr(commandsStartWith, 'commands'):
+        setattr(commandsStartWith, 'commands', {
+            '!countdown-': channel.commandCountdown,
+            }
+        )
+    return getattr(commandsStartWith, 'commands')
+
+
+def processNoCommand() -> Iterable[ChatCommand]:
+    return []
